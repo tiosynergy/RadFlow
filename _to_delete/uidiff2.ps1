0f30744a622811e6551ca78d72f56d8a4956eac5
tiosynergy <tiosynergy@gmail.com>
2026-07-28 20:04:18 +0300

    папка todelete почищена

--- _to_delete/uidiff2.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location D:\RadFlowDev
-$env:GIT_PAGER = 'cat'
-$out = & git --no-optional-locks diff -U1 -- styles/prototype/radflow.css components/RadiologistBoard.tsx 2>&1
-[System.IO.File]::WriteAllLines('D:\RadFlowDev\_to_delete\uidiff2.txt', $out, [System.Text.UTF8Encoding]::new($false))
